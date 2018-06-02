2bef678792fb24e5292e493c0188582be3d93f31
judell <jon@jonudell.info>
2018-06-01 19:48:12 -0700

    improve the build (sigh)

--- make.sh
+++ make.sh
@@ -1,23 +1,32 @@
-# tsc creates index.js and iframe.js
+# The TypeScript compiler converts index.ts and iframe.ts to index.js and iframe.js
 
 tsc
 
-# Those files contain:
+# The TypeScript files begin with this import:
 
   # import * as hlib from '../../hlib/hlib'
 
-# which makes hlib, now written in TypeScript, available to the editor.
+# That makes hlib, now written in TypeScript, available to the editor.
 
-# But apps that use hlib include it with a script tag.
+# But apps that use hlib are simple-minded and just want to include it with a script tag.
 
-# So this step transforms the tsc output, index.js and iframe..js, commenting out the import
-
-# // import * ...
+# So this step transforms the tsc outputs, inserting comments before the import statement
 
 python comment-out-imports.py
 
+# For local testing, three files need reference adjustment:
+#   index.html
+#   iframe.html
+#   showAnnotation.js
+# 
+# These are the options:
+#   dev: http://10.0.0.9:8000/hlib.bundle.js
+#   prd: https://jonudell.info/hlib/hlib.bundle.js
+
 if [ $1 = "dev" ]; then
   python use-debug-lib.py
-else
+fi
+
+if [ $1 = 'prd' ]; then
   python use-production-lib.py
 fi

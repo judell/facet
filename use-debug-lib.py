406d57bedbfef348bc5c33315fbb77073a42a095
judell <jon@jonudell.info>
2021-06-22 14:41:53 -0700

    use hlib3.bundle.js

--- use-debug-lib.py
+++ use-debug-lib.py
@@ -6,7 +6,7 @@ filenames = [
 for filename in filenames: 
     with open(filename, 'r+') as f:
         text = f.read()
-        text = text.replace('https://jonudell.info/hlib/hlib2.bundle.js', 'http://localhost:8000/hlib2.bundle.js')
+        text = text.replace('https://jonudell.info/hlib/hlib3.bundle.js', 'http://localhost:8000/hlib3.bundle.js')
         text = text.replace('https://jonudell.info/hlib/hlib.css', 'http://localhost:8000/hlib.css')        
         f.seek(0)
         f.write(text)
